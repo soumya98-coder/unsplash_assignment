01945d545b46b82487eec1c4bd534245996954fd
Soumya Ranjan <padhysoumya98@gmail.com>
2021-03-08 19:21:57 +0530

    asssignment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import Layout from "./Components/Layout/Layout";
+import PhotoContainer from "./Containers/PhotoContainer/PhotoContainer";
+import { authCheckState } from './store/actions/index'
 import './App.css';
 
 function App() {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(authCheckState());
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout>
+        <PhotoContainer />
+      </Layout>
     </div>
   );
 }
